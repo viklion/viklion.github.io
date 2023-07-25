d26be23e5b63ca304b63243f614af9c2a72eae61
viklion <52791134+viklion@users.noreply.github.com>
2023-07-25 14:12:45 +0800

    Update ipv4v6.js

--- js/ipv4v6.js
+++ js/ipv4v6.js
@@ -1,5 +1,5 @@
 function ipv4v6(data) {
-		if(data && data.type && data.type=='ipv6' && window.location.href.substr(0,v6url.length)!=v6url){
+		if(data && data.type && data.type=='ipv6'){
 			window.open('https://v6.alist.viklion.cn');
 		}
 		else{
